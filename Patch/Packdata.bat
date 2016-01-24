779fc1d0c06eff95628c8a6748c2f7445065ed20
wmltogether <wmltogether@gmail.com>
2016-01-24 11:55:31 +0800

    Create Packdata.bat

---
+++ Patch/Packdata.bat
@@ -0,0 +1,5 @@
+SARCPack.py permanent_2d_UsEnglish 8192
+ren permanent_2d_UsEnglish.sarc permanent_2d_UsEnglish.pack
+SARCPack.py Title_00 8192
+yaz0enc.exe Title_00.sarc
+ren Title_00.sarc.yaz0 Title_00.szs
